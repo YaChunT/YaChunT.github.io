789bfef4474dc608648bdc2f145aabda3d69d857
YaChunT <tyg0958851124@gmail.com>
2024-05-17 21:42:17 +0200

    update deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://YaChunT.github.io',
+  site: 'https://yachunt.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
